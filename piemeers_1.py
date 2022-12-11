f638ee0683c59c57fb7deafa68175a0a5eba6e59
annijara <99959888+annijara@users.noreply.github.com>
2022-12-11 18:29:18 +0200

    Update piemeers_1.py

--- piemeers_1.py
+++ piemeers_1.py
@@ -1,6 +1,17 @@
 import pandas
-print("Helloo")
+
+excel_fails = "dzivnieki.xls"
+#dati = pandas.read_excel(excel_fails)
+#print(dati["Suga"].str.len())
+
+# t = "MANS TEKSTS"
+# print(t.lower())
 
 print("\n\nvisu uz reizi:")
-viss_fails = panda.ExcelFile(excel_fails)
-print(viss_fails)
+viss_fails = pandas.ExcelFile(excel_fails)
+#print (viss_fails)
+lapas = []
+for lapa in viss_fails.sheet_names:
+    lapas.append(viss_fails.parse(lapa))
+print(lapas[0].info())
+print(lapas[1])
