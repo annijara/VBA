916aad98c67717a461c938baee3f6470ef5c8daa
annijara <99959888+annijara@users.noreply.github.com>
2022-12-11 19:01:21 +0200

    kk

--- piemers_2.py
+++ piemers_2.py
@@ -1,1 +1,42 @@
+import pandas
+fails = pandas.ExcelFile("dati_masiviem.xlsx")
+lapas = []
+for lapa in fails.sheet_names:
+    lapas.append(fails.parse(lapa))
+print(lapas[0].shape)
+print(lapas[0]["Nosaukums"]) 
+lapas[0]["Cena"] = lapas[0]["Pašizmaksa"]* 1.21
+#print(lapas[0]["Cena"])
+
+ieliekama_rinda = lapas[0][["Skaits","Cena"]].sum()
+#print("ieliekama rinda", ieliekama_rinda)
+parvietota_rinda = pandas.DataFrame(data = ieliekama_rinda).T
+print("----------")
+#print(parvietota_rinda)
+parvietota_rinda = parvietota_rinda.reindex(columns=lapas[0].columns)
+#print(parvietota_rinda)
+
+lapas.append(lapas[0])
+lapas[1] = lapas[1].append(parvietota_rinda)
+#print(lapas[1])
+
+grupetie_dati = lapas[0][["Datums", "Skaits"]].groupby('Datums').sum()
+#print(grupetie_dati)
+
+grupetie_dati.insert(0,'Datums2', grupetie_dati.index)
+#print(grupetie_dati)
+lapas.append(grupetie_dati)
+
+atrasts = lapas[2]["Datums2"] == "2020-09-09"
+print(lapas[2][atrasts])
+
+lapas_nr = 1
+with pandas.ExcelWriter("jauns_fails.xlsx") as fails:
+    for lapa in lapas:
+        lapa.to_excel(fails, sheet_name=str(lapas_nr), index=False)
+        lapas_nr +=1
+
+
+
+
 
